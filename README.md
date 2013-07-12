3b9e479e3cbf6e53698b287d3db82fdca67f49f8
Ron Rothman <ron.rothman@cbsinteractive.com>
2013-07-12 10:49:43 -0400

    latest

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 pugip-bottle-demo
 =================
+We built this webapp in under an hour as part of a demo for
+the meetup event "[Bottle: When Small is Good](http://www.meetup.com/pug-ip/events/124708312/)."
+
+The webapp is meant to expose a simple API over a corpus of zip codes and
+their associated data.
+
+**This file represents the [annotated] final version, so it doesn't include
+several of the intermediate teaching points I included during its construction.**
+
+It was created quickly, without any deep regard for extensibility.
+It's meant to be instructive, not realistic.  For example, there
+is no error checking, only minimal data validation, and the Bottle
+variables are fully qualified (e.g., "bottle.HTTPError" instead of
+"HTTPError") to help make explicit where Bottle is involved.
+
+Some usage:
+
+    python ./zipserver.py
+
+    http://127.0.0.1:8888/
+
+    http://127.0.0.1:8888/population/08540
+    http://127.0.0.1:8888/population/08540?all=1
+    http://127.0.0.1:8888/population/08540?all=0
+
+    http://127.0.0.1:8888/population/notazip
+    http://127.0.0.1:8888/population/999
+
+    http://127.0.0.1:8888/update-population/08540/100
+    http://127.0.0.1:8888/update-population/08540/-100
+    http://127.0.0.1:8888/update-population/08540/-100
+    curl --data 'zip_code=08540&delta=100' 'http://127.0.0.1:8888/update-population'
+    curl --data '{"zip_code": "08540", "delta": 100}' 'http://127.0.0.1:8888/update-population'
+    curl -H 'Content-type: application/json' --data '{"zip_code": "08540", "delta": 100}' 'http://127.0.0.1:8888/update-population'
+
+    http://127.0.0.1:8888/browse
+
--- zipserver_annotated.py
+++ zipserver_annotated.py
@@ -1,12 +1,12 @@
 '''
 We built this webapp in under an hour as part of a demo for
-this meetup event: http://www.meetup.com/pug-ip/events/124708312/
+the meetup event "[Bottle: When Small is Good](http://www.meetup.com/pug-ip/events/124708312/)."
 
 The webapp is meant to expose a simple API over a corpus of zip codes and
 their associated data.
 
-This file represents the [annotated] final version, so it doesn't include
-several of the intermediate teaching points I included during its construction.
+**This file represents the [annotated] final version, so it doesn't include
+several of the intermediate teaching points I included during its construction.**
 
 It was created quickly, without any deep regard for extensibility.
 It's meant to be instructive, not realistic.  For example, there
@@ -16,25 +16,25 @@ variables are fully qualified (e.g., "bottle.HTTPError" instead of
 
 Some usage:
 
-python ./zipserver.py
+    python ./zipserver.py
 
-http://127.0.0.1:8888/
+    http://127.0.0.1:8888/
 
-http://127.0.0.1:8888/population/08540
-http://127.0.0.1:8888/population/08540?all=1
-http://127.0.0.1:8888/population/08540?all=0
+    http://127.0.0.1:8888/population/08540
+    http://127.0.0.1:8888/population/08540?all=1
+    http://127.0.0.1:8888/population/08540?all=0
 
-http://127.0.0.1:8888/population/notazip
-http://127.0.0.1:8888/population/999
+    http://127.0.0.1:8888/population/notazip
+    http://127.0.0.1:8888/population/999
 
-http://127.0.0.1:8888/update-population/08540/100
-http://127.0.0.1:8888/update-population/08540/-100
-http://127.0.0.1:8888/update-population/08540/-100
-curl --data 'zip_code=08540&delta=100' 'http://127.0.0.1:8888/update-population'
-curl --data '{"zip_code": "08540", "delta": 100}' 'http://127.0.0.1:8888/update-population'
-curl -H 'Content-type: application/json' --data '{"zip_code": "08540", "delta": 100}' 'http://127.0.0.1:8888/update-population'
+    http://127.0.0.1:8888/update-population/08540/100
+    http://127.0.0.1:8888/update-population/08540/-100
+    http://127.0.0.1:8888/update-population/08540/-100
+    curl --data 'zip_code=08540&delta=100' 'http://127.0.0.1:8888/update-population'
+    curl --data '{"zip_code": "08540", "delta": 100}' 'http://127.0.0.1:8888/update-population'
+    curl -H 'Content-type: application/json' --data '{"zip_code": "08540", "delta": 100}' 'http://127.0.0.1:8888/update-population'
 
-http://127.0.0.1:8888/browse
+    http://127.0.0.1:8888/browse
 '''
 
 import bottle
